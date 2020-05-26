e1470aac6f30298e424becf74e2a85cf80f65351
William Whinn <w.willis.1986@gmail.com>
2020-05-26 10:45:00 +0100

    Format URL string in --version

--- src/main.c
+++ src/main.c
@@ -68,8 +68,9 @@ int main(int argc, char *argv[])
 		printf(PROGRAM_VERSION);
 		printf("\n");
 		printf("Copyright (C) 2020 William Whinn\n");
+		printf("<");
 		printf(PROGRAM_URL);
-		printf("\n");
+		printf(">\n");
 		printf("License GPLv3+: GNU GPL version 3 or later "
 				"<https://gnu.org/licenses/gpl.html>.\n");
 		printf("This is free software: you are free to change and "
